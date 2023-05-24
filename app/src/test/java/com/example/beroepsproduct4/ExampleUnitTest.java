84a202d33ddd3fb0abba9d60dc6047e40f9b4061
gabriellespits <gabrielle-spits@hotmail.com>
2023-05-24 09:55:18 +0200

    unittest

--- app/src/test/java/com/example/beroepsproduct4/ExampleUnitTest.java
+++ app/src/test/java/com/example/beroepsproduct4/ExampleUnitTest.java
@@ -3,6 +3,11 @@ package com.example.beroepsproduct4;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import com.example.beroepsproduct4.model.Oudergegevens;
+import com.example.beroepsproduct4.model.Rollatorhoortbij;
+import com.example.beroepsproduct4.model.Zorgcentrum;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -14,4 +19,28 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+    @Test
+    public void Zorcentrum_zorgcentrum(){
+        Zorgcentrum z = new Zorgcentrum();
+        z.setZorgcentrum("test");
+        assertEquals("test",z.getZorgcentrum());
+    }
+
+    @Test
+    public void Oudergegevens_oudernaam() {
+        Oudergegevens o = new Oudergegevens();
+        o.setOudernaam("Piet");
+        assertEquals("Piet",o.getOudernaam());
+    }
+
+    @Test
+    public void Rollatorhoortbij_oudergegevens(){
+        Oudergegevens o = new Oudergegevens("bsn");
+        Rollatorhoortbij r = new Rollatorhoortbij();
+        r.setOudergegevens(o);
+        assertEquals(o,r.getOudergegevens());
+
+    }
+
+
 }
